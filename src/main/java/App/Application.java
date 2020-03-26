409aca5257aaad44c5837eae01e6fa1e603720da
KotsinskyiNazar <zemlya2023@gmail.com>
2020-03-26 18:17:05 +0200

    ver 0.01

--- src/main/java/App/Application.java
+++ src/main/java/App/Application.java
@@ -4,6 +4,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("Nazar");
+        System.out.println("How are U?");
     }
 
 }
